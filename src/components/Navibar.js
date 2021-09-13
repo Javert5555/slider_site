a4b78186b52d5c2b2b59df6285e26a09828157a6
Maxwell Rutherford <javert.hugo@yandex.ru>
2021-09-13 23:35:42 +0300

    modified navibar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Navibar = () => (
     <nav className='nav'>
-        <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='/contact'>Contact</Link></li>
-        </ul>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
     </nav>
 );
 
